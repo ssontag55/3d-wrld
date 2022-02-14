6cad9c1921dba0ca1230f76278979071669059bf
Stephen Sontag <stephen.sontag@agoramaps.com>
2022-02-14 17:49:17 -0500

    main 3d demo

---
+++ exceedance.js
@@ -0,0 +1,192 @@
+var exceedanceSetJson = [
+  {
+    "param": "temp",
+    "unit": "Deg F",
+    "level": 2,
+    "lower": "78",
+    "upper": null,
+    "background_color": "#FFA500",
+    "text_color": "#000000",
+    "status": "High"
+  },
+  {
+    "param": "temp",
+    "unit": "Deg F",
+    "level": 0,
+    "lower": null,
+    "upper": "68",
+    "background_color": "#00FFFF",
+    "text_color": "#000000",
+    "status": "Low"
+  },
+  {
+    "param": "temp",
+    "unit": "Deg F",
+    "level": 1,
+    "lower": "68",
+    "upper": "78",
+    "background_color": "#009966",
+    "text_color": "#FFFFFF",
+    "status": "Typical"
+  },
+  {
+    "param": "pm_10",
+    "unit": "ug/m3",
+    "level": 1,
+    "lower": "75",
+    "upper": "150",
+    "background_color": "#ffde33",
+    "text_color": "#000000",
+    "status": "Moderate"
+  },
+  {
+    "param": "pm_10",
+    "unit": "ug/m3",
+    "level": 0,
+    "lower": null,
+    "upper": "75",
+    "background_color": "#009966",
+    "text_color": "#FFFFFF",
+    "status": "Typical"
+  },
+  {
+    "param": "pm_2_5",
+    "unit": "ug/m3",
+    "level": 1,
+    "lower": "17.5",
+    "upper": "35",
+    "background_color": "#ffde33",
+    "text_color": "#000000",
+    "status": "Moderate"
+  },
+  {
+    "param": "pm_2_5",
+    "unit": "ug/m3",
+    "level": 0,
+    "lower": null,
+    "upper": "17.5",
+    "background_color": "#009966",
+    "text_color": "#FFFFFF",
+    "status": "Typical"
+  },
+  {
+    "param": "co2",
+    "unit": "ppm",
+    "level": 1,
+    "lower": "1000",
+    "upper": "2000",
+    "background_color": "#ffde33",
+    "text_color": "#000000",
+    "status": "Moderate"
+  },
+  {
+    "param": "co2",
+    "unit": "ppm",
+    "level": 0,
+    "lower": null,
+    "upper": "1000",
+    "background_color": "#009966",
+    "text_color": "#FFFFFF",
+    "status": "Typical"
+  },
+  {
+    "param": "co",
+    "unit": "ppm",
+    "level": 2,
+    "lower": "9",
+    "upper": null,
+    "background_color": "#FFA500",
+    "text_color": "#000000",
+    "status": "High"
+  },
+  {
+    "param": "co",
+    "unit": "ppm",
+    "level": 1,
+    "lower": "4",
+    "upper": "9",
+    "background_color": "#ffde33",
+    "text_color": "#000000",
+    "status": "Moderate"
+  },
+  {
+    "param": "co",
+    "unit": "ppm",
+    "level": 0,
+    "lower": null,
+    "upper": "4",
+    "background_color": "#009966",
+    "text_color": "#FFFFFF",
+    "status": "Typical"
+  },
+  {
+    "param": "humidity",
+    "unit": "%",
+    "level": 2,
+    "lower": "60",
+    "upper": null,
+    "background_color": "#FFA500",
+    "text_color": "#000000",
+    "status": "High"
+  },
+  {
+    "param": "pressure",
+    "unit": "mB",
+    "level": 0,
+    "lower": "-10000",
+    "upper": "10000",
+    "background_color": "#009966",
+    "text_color": "#FFFFFF",
+    "status": "Typical"
+  },
+  {
+    "param": "pm_10",
+    "unit": "ug/m3",
+    "level": 2,
+    "lower": "150",
+    "upper": null,
+    "background_color": "#FFA500",
+    "text_color": "#000000",
+    "status": "High"
+  },
+  {
+    "param": "pm_2_5",
+    "unit": "ug/m3",
+    "level": 2,
+    "lower": "35",
+    "upper": null,
+    "background_color": "#FFA500",
+    "text_color": "#000000",
+    "status": "High"
+  },
+  {
+    "param": "co2",
+    "unit": "ppm",
+    "level": 2,
+    "lower": "2000",
+    "upper": null,
+    "background_color": "#FFA500",
+    "text_color": "#000000",
+    "status": "High"
+  },
+  {
+    "param": "humidity",
+    "unit": "%",
+    "level": 0,
+    "lower": null,
+    "upper": "30",
+    "background_color": "#00FFFF",
+    "text_color": "#000000",
+    "status": "Low"
+  },
+  {
+    "param": "humidity",
+    "unit": "%",
+    "level": 1,
+    "lower": "30",
+    "upper": "60",
+    "background_color": "#009966",
+    "text_color": "#FFFFFF",
+    "status": "Typical"
+  }
+]
